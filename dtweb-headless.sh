f7d31ea34baf0ea390fa403282673c1636ea3bf7
Francesco Montefoschi <francesco.monte@gmail.com>
2016-11-11 11:25:42 +0100

    Added dtweb-headless

---
+++ dtweb-headless.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /opt/dtweb/public/
+php -S 0.0.0.0:7533
--- dtweb.sh
+++ dtweb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/dtweb/public/
-php -S localhost:7533 &
+php -S 0.0.0.0:7533 &
 PHP_PID=$!
 echo $PHP_PID
 su -c "chromium-browser http://localhost:7533" udooer
